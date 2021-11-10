eb8fbc23e77e60e98493d4c3ed9ed386f44bb602
Troy <troyd9216@gmail.com>
2021-11-09 20:30:27 -0600

    basic functionality complete

--- canvas/src/App.js
+++ canvas/src/App.js
@@ -28,14 +28,14 @@ const notesReducer = (prevState, action) => {
       console.log('after delete', newState)
       return newState
     }
-    case "UPDATE_POS": {
-      const newState = {
-        ...prevState,
-        notes: [...prevState.notes, action.payload]
-      }
-      console.log('persist', newState)
-      return newState
-    }
+    // case "UPDATE_POS": {
+    //   const newState = {
+    //     ...prevState,
+    //     notes: [...prevState.notes, action.payload]
+    //   }
+    //   console.log('persist', newState)
+    //   return newState
+    // }
     case "CLEAR": {
       const newState = {
         ...prevState,
@@ -62,7 +62,8 @@ function App() {
     const newNote = {
       id: uuid(),
       text: noteInput,
-      defaultPos: { x: 100, y: 0 }
+      defaultPos: { x: 100, y: 0 },
+      color: "bg-yellow-200"
     };
     console.log(newNote);
     dispatch({ type: "ADD_NOTE", payload: newNote });
@@ -72,24 +73,24 @@ function App() {
   return (
     <>
 
-      <div class="container w-full max-w-xs mx-auto mt-8 px-4 font-sans">
+      <div className="container w-full max-w-xs mx-auto mt-8 px-4 font-sans">
         <form
-          class="bg-yellow-200 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"
+          className="bg-yellow-200 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"
           onSubmit={addNote}
         >
           <textArea
             value={noteInput}
             onChange={(event) => setNoteInput(event.target.value)}
-            placeholder="Create new note..." class="bg-yellow-200 appearance-none border-none border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-200"
+            placeholder="Create new note..." className="bg-yellow-200 appearance-none border-none border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-200"
           ></textArea>
           <button>Add</button>
         </form>
         {notesState.notes.map((note, index) => {
           return (
             <Draggable key={note.id} defaultPosition={note.defaultPos} onStop={(e, note) => {
-              dispatch({ type: "UPDATE_POS", payload: note })
+              // dispatch({ type: "UPDATE_POS", payload: note })
             }}>
-              <div class={"bg-yellow-200 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"}>
+              <div className={`${note.color} shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 break-words`}>
                 {note.text}
                 <button onClick={() => dispatch({ type: "DELETE_NOTE", payload: note })}>
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
