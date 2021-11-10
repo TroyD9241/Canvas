c2eb8da2cca6670d68ad31abb30cd40fa5822777
Troy <troyd9216@gmail.com>
2021-11-09 19:26:55 -0600

    stickies can be dragged and dropped

--- canvas/src/App.js
+++ canvas/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useReducer } from "react";
 import { v4 as uuid } from "uuid";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import Draggable from "react-draggable";
 import "./index.css";
 
 const initialState = {
   id: null,
   lastNoteCreated: null,
   notes: [],
+  defaultPos: { x: 100, y: 0 }
 };
 
 const notesReducer = (prevState, action) => {
@@ -27,8 +28,15 @@ const notesReducer = (prevState, action) => {
       console.log('after delete', newState)
       return newState
     }
+    case "CLEAR": {
+      const newState = {
+        ...prevState,
+        notes: []
+      }
+      return newState
+    }
     default:
-      break;
+      return initialState
   }
 };
 
@@ -46,46 +54,43 @@ function App() {
     const newNote = {
       id: uuid(),
       text: noteInput,
-      rotate: Math.floor(Math.random() * 100),
     };
     console.log(newNote);
     dispatch({ type: "ADD_NOTE", payload: newNote });
   };
 
-  const dropNote = () => {
-  }
-
-  const dragOver = (event) => {
-    event.stopPropagation()
-    event.preventDefault()
-  }
-
   return (
-    <div class="container w-full max-w-xs mx-auto mt-8 px-4 font-sans" onDragOver={dragOver}>
+    <>
 
-      <form
-        class="bg-yellow-200 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"
-        onSubmit={addNote}
-      >
-        <textArea
-          value={noteInput}
-          onChange={(event) => setNoteInput(event.target.value)}
-          placeholder="Create new note..." class="bg-yellow-200 appearance-none border-none border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-200"
-        ></textArea>
-        <button>Add</button>
-      </form>
-      {notesState.notes.map(note => {
-        return <div class={`bg-yellow-200 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 transform rotate-0 ${note.rotate}`}>
-          draggable="true"
-          onDragEnd={dropNote}
-          key={note.id}
-          {note.text}
-          <button onClick={() => dispatch({ type: "DELETE_NOTE", payload: note })}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-          </svg></button>
-        </div>
-      })}
-    </div >
+      <div class="container w-full max-w-xs mx-auto mt-8 px-4 font-sans">
+        <form
+          class="bg-yellow-200 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"
+          onSubmit={addNote}
+        >
+          <textArea
+            value={noteInput}
+            onChange={(event) => setNoteInput(event.target.value)}
+            placeholder="Create new note..." class="bg-yellow-200 appearance-none border-none border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-200"
+          ></textArea>
+          <button>Add</button>
+        </form>
+        {notesState.notes.map(note => {
+          return (
+            <Draggable key={note.id} defaultPosition={note.defaultPos}>
+              <div class={"bg-yellow-200 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"}>
+                {note.text}
+                <button onClick={() => dispatch({ type: "DELETE_NOTE", payload: note })}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                  </svg></button>
+              </div>
+            </Draggable>
+          )
+        })}
+      </div >
+      <button onClick={() => dispatch({ type: "CLEAR" })}>clear</button>
+
+    </>
   );
 }
 
